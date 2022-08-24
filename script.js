f41a6e04a32ba8c8cbad5335dd2ef526dd8f60de
dainabo <daina.bogdanova@gmail.com>
2022-08-24 17:42:38 +0300

    add 2nd task

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const addNumbers = function (a, b) {
     return a + b;
 }
 // console.log(addNumbers(3, 5));
+
+//2. Write a function that takes a string as an argument. Extract the last 3 characters from the string. Return the result
+
+const lastCharacters = function (string) {
+    return string.slice(-3);
+}
+// console.log(lastCharacters("sometext"));
+
+// 3. Write a function that takes a number (a) as argument Split a into its individual digits and return them in an array.
+
+// function splitNumbers(a){
+//     return String(a).split("1234567").map(Number);
+//     }
+
+const splitNums = function (a) {
+    return String(a).split("1234567").map(Number);
+}
+console.log(Number);
