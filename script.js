dad8098d585e14353c7eb8a8ac88f0bcabbb5169
dainabo <daina.bogdanova@gmail.com>
2022-08-24 18:28:32 +0300

    update task description

--- script.js
+++ script.js
@@ -12,3 +12,7 @@ const lastCharacters = function (string) {
 // console.log(lastCharacters("sometext"));
 
 // 3. Write a function that takes a number (a) as argument Split a into its individual digits and return them in an array.
+
+// 4. Write a function that takes an array (a) and a number (n) as arguments It should return the last n elements of a
+// 5. Write a function that takes two arrays as arguments Merge both arrays and remove duplicate values Sort the merge result in ascending order Return the resulting array
+// 6. Write a function that takes an object (a) and a string (b) as argument  Return true if the object has a property with key 'b' Return false otherwise
